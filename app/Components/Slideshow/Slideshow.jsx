fecb2563e23e85510f920d2dde185200f37e6ebb
Hsen-123 <hazimehussein01@gmail.com>
2023-12-27 13:04:57 +0200

    add image slideshow

---
+++ app/Components/Slideshow/Slideshow.jsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import "./style.css";
+
+export default function Slideshow({ images }) {
+  const repeatedImages = [...images, ...images, ...images];
+
+  return (
+    <div className="overflow-hidden pb-10">
+      <div className="flex">
+        <div className="ml-4 flex justify-center gap-4 logos-slide-container">
+          {repeatedImages.map((image, i) => (
+            <div key={i} className="logos-slide">
+              <div className="w-full">
+                <div className="flex flex-col items-center justify-between w-full ">
+                  <h5 className="">
+                    <Image
+                      alt="profile"
+                      className="max-h-[200px] max-w-[300px] md:max-w-[500px] md:max-h-[400px] object-contain "
+                      width={500}
+                      height={300}
+                      objectFit="contain"
+                      src={image}
+                    />
+                  </h5>
+                </div>
+              </div>
+            </div>
+          ))}{" "}
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,10 +1,20 @@
-import Image from "next/image";
 import Hero from "./Components/Hero";
+import Slideshow from "./Components/Slideshow/Slideshow";
+
+const images = [
+  "/slide-1.webp",
+  "/slide-2.webp",
+  "/slide-3.webp",
+  "/slide-4.webp",
+  "/slide-5.webp",
+  "/slide-6.webp",
+];
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Slideshow images={images} />
     </>
   );
 }
