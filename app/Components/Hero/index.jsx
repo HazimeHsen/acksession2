e7853860a9da5268c57081a09ccb989b5b4bacc8
Hsen-123 <hazimehussein01@gmail.com>
2024-01-01 20:26:46 +0200

    .

--- app/Components/Hero/index.jsx
+++ app/Components/Hero/index.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useRef } from "react";
 import Button from "../Button";
 import { FaArrowRight } from "react-icons/fa";
 import { motion, useAnimation, useInView } from "framer-motion";
+import Animation from "../Animation";
 const Hero = () => {
   const controls = useAnimation();
   const ref = useRef(null);
@@ -15,44 +16,34 @@ const Hero = () => {
   }, [controls, isInView]);
   return (
     <div className="md:pt-10 flex justify-center items-center h-screen text-center flex-col gap-5 md:gap-10 w-full px-4 md:px-10">
-      <motion.div
-        ref={ref}
-        initial="hidden"
-        animate={controls}
-        variants={{
-          visible: { opacity: 1, y: 0 },
-          hidden: { opacity: 0, y: 50 },
-        }}
-        transition={{ duration: 0.3 }}
+      <Animation
+        startInView={false}
+        animationType="fade-bottom"
+        duration={0.3}
+        delay={0.3}
         className="text-4xl xs:text-5xl md:text-6xl lg:text-7xl font-bold text-primary capitalize max-w-4xl">
         Leading <span className="text-secondary">web & mobile</span> application
         development company
-      </motion.div>
-      <motion.div
-        initial="hidden"
-        animate={controls}
-        variants={{
-          visible: { opacity: 1, y: 0 },
-          hidden: { opacity: 0, y: 50 },
-        }}
-        transition={{ duration: 0.3, delay: 0.3 }}
+      </Animation>
+      <Animation
+        startInView={false}
+        animationType="fade-bottom"
+        duration={0.3}
+        delay={0.6}
         className="text-gray max-w-xl text-sm md:text-lg font-semibold">
         We specialize in offering comprehensive web and mobile application
         development services to cater to the unique requirements of both budding
         startups and established enterprises.
-      </motion.div>
+      </Animation>
       <div className="flex md:flex-row flex-col w-full md:w-1/2 gap-4 items-center">
-        <motion.div
-          initial="hidden"
-          animate={controls}
-          variants={{
-            visible: { opacity: 1, y: 0 },
-            hidden: { opacity: 0, y: 50 },
-          }}
-          transition={{ duration: 0.3, delay: 0.6 }}
+        <Animation
+          startInView={false}
+          animationType="fade-bottom"
+          duration={0.3}
+          delay={0.9}
           className="w-full">
           <Button text="Get free quote" Icon={FaArrowRight} />
-        </motion.div>
+        </Animation>
       </div>
     </div>
   );
